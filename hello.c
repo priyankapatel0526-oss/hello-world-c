759c7d344f2fdfdc0bbe1bf31a867ce86e75aa07
Priyanka K V <priyankapatel0526@gmail.com>
2026-08-31 12:12:47 +0530

    Add greet() function

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
-int main()
-{
-    printf("hello world\n");
+#include <stdio.h>
+
+void greet(const char *name) {
+    printf("Hello, %s! Welcome to your GitHub portfolio.\n", name);
+}
+
+int main() {
+    printf("Hello World!\n");
+
+    greet("Ada");
+
     return 0;
 }
